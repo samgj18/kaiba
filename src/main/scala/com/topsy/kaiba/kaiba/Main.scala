5c5d16736b8a00c484a711f25d2ece79dd45eca3
samgj18 <samgj_18@hotmail.com>
2021-05-30 00:28:06 -0500

    Initial commit

---
+++ src/main/scala/com/topsy/kaiba/kaiba/Main.scala
@@ -0,0 +1,10 @@
+package com.topsy.kaiba
+package kaiba
+
+object Main extends App {
+  println("─" * 100)
+
+  println("hello world changes")
+
+  println("─" * 100)
+}
---
+++ src/test/scala/com/topsy/kaiba/kaiba/ExampleSuite.scala
@@ -0,0 +1,8 @@
+package com.topsy.kaiba
+package kaiba
+
+final class ExampleSuite extends TestSuite {
+  test("hello world") {
+    1 shouldBe 1
+  }
+}
