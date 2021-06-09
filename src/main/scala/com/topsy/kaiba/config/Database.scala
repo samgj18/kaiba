8186ab15e29aabfc83cc5930add49e33420fa7b3
calvinlfer <calvin.l.fer@gmail.com>
2021-06-09 17:10:47 -0400

    - Remove unused ZEnv dependency

--- src/main/scala/com/topsy/kaiba/config/Database.scala
+++ src/main/scala/com/topsy/kaiba/config/Database.scala
@@ -2,11 +2,11 @@ package com.topsy.kaiba.config
 
 import io.getquill._
 import zio.blocking.Blocking
-import zio.{ Has, ZLayer }
+import zio.{ Has, RLayer }
 
 object Database {
   object ZioCassandraContext extends CassandraZioContext(Literal)
 
-  lazy val layer: ZLayer[Blocking, Throwable, Has[CassandraZioSession]] =
+  lazy val layer: RLayer[Blocking, Has[CassandraZioSession]] =
     CassandraZioSession.fromPrefix("topsy_auth")
 }
